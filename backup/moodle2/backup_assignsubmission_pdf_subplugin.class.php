001d3716276b59afe9101d646f42d8a95f38193c
Davo Smith <moodle@davosmith.co.uk>
2012-12-31 21:26:30 +0000

    Backup and restore working (but not template contents)

---
+++ backup/moodle2/backup_assignsubmission_pdf_subplugin.class.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the class for backup of this submission plugin
+ *
+ * @package assignsubmission_pdf
+ * @copyright 2012 Davo Smith
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Provides the information to backup submission files
+ *
+ * This just adds its filearea to the annotations and records the number of files
+ *
+ * @package assignsubmission_pdf
+ * @copyright 2012 Davo Smith
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class backup_assignsubmission_pdf_subplugin extends backup_subplugin {
+
+    /**
+     *
+     * Returns the subplugin information to attach to submission element
+     * @return backup_subplugin_element
+     */
+    protected function define_submission_subplugin_structure() {
+
+        // create XML elements
+        $subplugin = $this->get_subplugin_element(); // virtual optigroup element
+        $subpluginwrapper = new backup_nested_element($this->get_recommended_name());
+        $subpluginelement = new backup_nested_element('submission_pdf', null,
+                                                      array('numpages', 'submission', 'status', 'templatedata'));
+
+        // Backup comments / annotations here, rather than in the 'feedback' backup
+        $comments = new backup_nested_element('comments');
+        $comment = new backup_nested_element('comment', null,
+                                             array('submissionid', 'posx', 'posy', 'width', 'rawtext', 'pageno', 'colour'));
+
+        $annotations = new backup_nested_element('annotations');
+        $annotation = new backup_nested_element('annotation', null,
+                                                array('submissionid', 'pageno', 'startx', 'starty', 'endx', 'endy',
+                                                     'path', 'colour', 'type'));
+
+        // connect XML elements into the tree
+        $subplugin->add_child($subpluginwrapper);
+        $subpluginwrapper->add_child($subpluginelement);
+        $subpluginelement->add_child($comments);
+        $comments->add_child($comment);
+        $subpluginelement->add_child($annotations);
+        $annotations->add_child($annotation);
+
+        // set source to populate the data
+        $subpluginelement->set_source_table('assignsubmission_pdf', array('submission' => backup::VAR_PARENTID));
+        $comment->set_source_table('assignfeedback_pdf_cmnt', array('submissionid' => backup::VAR_PARENTID));
+        $annotation->set_source_table('assignfeedback_pdf_annot', array('submissionid' => backup::VAR_PARENTID));
+
+        $subpluginelement->annotate_files('assignsubmission_pdf', 'submission_pdf_draft', 'submission');// The parent is the submission
+        $subpluginelement->annotate_files('assignsubmission_pdf', 'submission_pdf_final', 'submission');// The parent is the submission
+        $subpluginelement->annotate_files('assignsubmission_pdf', 'submission_pdf_coversheet', null);// No itemid for the coversheet
+        $subpluginelement->annotate_files('assignfeedback_pdf', 'feedback_pdf_response', 'submission');// The parent is the submission
+        return $subplugin;
+    }
+}
---
+++ backup/moodle2/restore_assignsubmission_pdf_subplugin.class.php
@@ -0,0 +1,97 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the class for restore of this submission plugin
+ *
+ * @package assignsubmission_pdf
+ * @copyright 2012 Davo Smith
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * restore subplugin class that provides the necessary information needed to restore one assign_submission subplugin.
+ *
+ * @package assignsubmission_pdf
+ * @copyright 2012 Davo Smith
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class restore_assignsubmission_pdf_subplugin extends restore_subplugin {
+
+    /**
+     * Returns the paths to be handled by the subplugin at workshop level
+     * @return array
+     */
+    protected function define_submission_subplugin_structure() {
+        $paths = array();
+
+        $elename = $this->get_namefor('submission');
+        $elepath = $this->get_pathfor('/submission_pdf'); // we used get_recommended_name() so this works
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        $elename = 'assignsubmission_pdf_comment';
+        $elepath = $this->get_pathfor('/submission_pdf/comments/comment');
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        $elename = 'assignsubmission_pdf_annotation';
+        $elepath = $this->get_pathfor('/submission_pdf/annotations/annotation');
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        return $paths; // And we return the interesting paths
+    }
+
+    /**
+     * Processes one submission_pdf element
+     * @param mixed $data
+     * @return void
+     */
+    public function process_assignsubmission_pdf_submission($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $data->assignment = $this->get_new_parentid('assign');
+        $oldsubmissionid = $data->submission;
+        // the mapping is set in the restore for the core assign activity. When a submission node is processed
+        $data->submission = $this->get_mappingid('submission', $data->submission);
+
+        $DB->insert_record('assignsubmission_pdf', $data);
+
+        $this->add_related_files('assignsubmission_pdf', 'submission_pdf_draft', 'submission', null, $oldsubmissionid);
+        $this->add_related_files('assignsubmission_pdf', 'submission_pdf_final', 'submission', null, $oldsubmissionid);
+        $this->add_related_files('assignsubmission_pdf', 'submission_pdf_coversheet', null);
+        $this->add_related_files('assignfeedback_pdf', 'feedback_pdf_response', 'submission', null, $oldsubmissionid);
+    }
+
+    public function process_assignsubmission_pdf_comment($data) {
+        global $DB;
+
+        $data = (object)$data;
+        // the mapping is set in the restore for the core assign activity. When a submission node is processed
+        $data->submissionid = $this->get_mappingid('submission', $data->submissionid);
+
+        $DB->insert_record('assignfeedback_pdf_cmnt', $data);
+    }
+
+    public function process_assignsubmission_pdf_annotation($data) {
+        global $DB;
+
+        $data = (object)$data;
+        // the mapping is set in the restore for the core assign activity. When a submission node is processed
+        $data->submissionid = $this->get_mappingid('submission', $data->submissionid);
+
+        $DB->insert_record('assignfeedback_pdf_annot', $data);
+    }
+}
